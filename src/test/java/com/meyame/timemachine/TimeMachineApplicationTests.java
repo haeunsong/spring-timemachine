a8c30777b7ccd9b230343603db5cb45848b33004
haeunsong <jamy022576@gmail.com>
2024-11-15 09:50:14 +0900

    initial commit

---
+++ src/test/java/com/meyame/timemachine/TimeMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meyame.timemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
